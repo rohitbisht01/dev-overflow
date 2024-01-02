41c035cdc0d547c4e4e01441987da563376056be
rohitbisht01 <rb9519208@gmail.com>
2024-01-02 23:58:34 +0530

    feat: added local search bar and tags

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,64 @@
-import React from "react";
+import HomeFilters from "@/components/home/HomeFilter";
+import Filter from "@/components/shared/Filters";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
 
-export default function page() {
-  return <div>Home</div>;
+export default async function Home() {
+  // const result = await getQuestions({});
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no question to show"
+            description="Be the first to break the silence! Ask a question and kickstart the discucssion. out query could be the next big thing others lean from. Get involved"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )} */}
+      </div>
+    </>
+  );
 }
