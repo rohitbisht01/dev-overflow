7ecf3dcb84bc34ce4484deea43f13146260777d5
rohitbisht01 <rb9519208@gmail.com>
2024-01-02 23:48:40 +0530

    updated util.ts fuke

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,36 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - createdAt.getTime();
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  } else if (hours > 0) {
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  } else {
+    return `${seconds} ${seconds === 1 ? "second" : "seconds"} ago`;
+  }
+};
+
+export const formatAndDivideBigNumber = (number: number): string => {
+  if (Math.abs(number) >= 1e6) {
+    return (number / 1e6).toFixed(1) + "M";
+  } else if (Math.abs(number) >= 1e3) {
+    return (number / 1e3).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
+};
