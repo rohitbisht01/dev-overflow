d17d65357ccce9368ad8610c61b805f72aa544a5
rohitbisht01 <rb9519208@gmail.com>
2024-01-04 03:30:17 +0530

    refactor: added type safety

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const page = async ({ params, searchParams }) => {
+const page = async ({ params: any }) => {
   const result = await getQuestionById({ questionId: params.id });
   const { userId: clerkId } = auth();
 
